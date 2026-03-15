76870383f29c93def5238289abf5f2f256d1ccd5
raiton685 <nemesis6850@gmail.com>
2026-03-15 17:48:36 +0300

    15.03

---
+++ src/Program/Analyzer.java
@@ -0,0 +1,64 @@
+package Program;
+
+import java.util.Scanner;
+
+public class Analyzer {
+    Scanner in = new Scanner(System.in);
+    int number = in.nextInt();
+
+    public void evenOrNot() {
+        if (number % 2 == 0) {
+            System.out.println("Число: " + number + " - чётное.");
+        } else {
+            System.out.println("Число: " + number + " - нечётное.");
+        }
+    }
+
+    public void numSign() {
+        if (number > 0) {
+            System.out.println("\n" + "Число: " + number + " - положительное.");
+        } else if (number < 0) {
+            System.out.println("\n" + "Число: " + number + " - отрицательное.");
+        } else {
+            System.out.println("\n" + "Число является нулём");
+        }
+    }
+
+    public void howManyNumbers() {
+
+        int result = 0;
+        for (int i = number; i > 0; i /= 10) {
+            result++;
+        }
+        System.out.println("\n" + "Количество цифр в указанном числе: " + result);
+    }
+
+    public void sumRange() {
+        int start = 1;
+        int result = 0;
+        for (int i = start; i <= number; i++) {
+            result += i;
+        }
+        System.out.println("\n" + "Сумма чисел от 1 до " + number + ": " + result);
+    }
+
+    public void multiplication() {
+        System.out.println("\n" + "Таблица умножения для числа: " + number + "\n");
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            System.out.println(number + " * " + i + " = " + result);
+        }
+    }
+}
+
+class ProgramTest {
+    public static void main(String[] args) {
+        System.out.print("Введите число: ");
+        Analyzer program = new Analyzer();
+        program.evenOrNot();
+        program.numSign();
+        program.howManyNumbers();
+        program.sumRange();
+        program.multiplication();
+    }
+}
